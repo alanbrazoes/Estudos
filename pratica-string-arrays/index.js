51321ac4d9f794288efa3000c6b5894d987b56fc
Alan Brazões <alanbrazoes@gmail.com>
2021-12-02 09:20:20 -0300

    ajustes

--- pratica-string-arrays/index.js
+++ pratica-string-arrays/index.js
@@ -1,25 +1,9 @@
-// EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO
-//Exercício 1: a. Undefined 
-// b. null 
-// c. 11
-// d. 3
-// e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
-// f. 9
-
-//exercício 2:
-// SUBA NUM ONBUS EM MIRROCOS 27
-// Fiz a correção, pois interpretei errado o replaceaall e não me lembrava que o .langth contava os espaços.
-
-
-// Exercício de escrita de código
-
 //Exercício 1
 
 const nomeDoUsuario = prompt("Digite seu nome.")
 const emailDoUsuario = prompt("Digite seu e-mail.")
 
 console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
-//Fiz a correção do tipo de concatenação com o dolar.
 
 //Exercício 2
 
@@ -57,4 +41,3 @@ let jaRealizado = prompt("Qual das três tarefas você já cumpriu? (Digite em N
 
 let retirandoTarefa = listaDeTarefas1.splice(Number(jaRealizado, 1))
 console.log(listaDeTarefas)
- // Fiz uma pequena correção na linha 58 convertendo em Number.
